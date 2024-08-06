ded66b002b0be85af4f2c6aae4509bd8caeb16ff
Erika Stuart (1000106562) <erikat.stuart@gmail.com>
2024-08-07 10:16:50 +1200

    Added: Less moving blocks, with gradience!

--- Assets/Scripts/MovingBackground.cs
+++ Assets/Scripts/MovingBackground.cs
@@ -3,32 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
-/// A list of background gameobjects will move downwards until they reach a point, where they will start at the top again.
-/// This will be attached to each sprite so that they can move independently.
+/// The first background should move down.
+/// If it reached the end point, it should be destroyed.
+/// The second background should continue to move down until it reaches y=0
+/// then the movement stops.
 /// </summary>
 public class MovingBackground : MonoBehaviour
 {
     private float speed = 1.0f;
-    private float resetPoint = -6.0f;
-    private float startPoint = 6.0f;
+    private float endPoint = -12.0f;
 
     void Start()
     {
-        StartCoroutine(MoveBackground());
+
     }
-    
-    IEnumerator MoveBackground()
+
+    void Update()
     {
-        while (true) // Infinite loop to keep the background moving
+        MoveDown();
+        
+        if (gameObject.name == "Horizon Background" && transform.position.y <= endPoint)
         {
-            transform.position += Vector3.down * speed * Time.deltaTime; // Move the background downwards
+            Destroy(gameObject);
+        }
 
-            if (transform.position.y <= resetPoint) // If the position of the sprites is less than the reset point
-            {
-                transform.position = new Vector3(transform.position.x, startPoint, transform.position.z); // Move it to the start point
-            }
-
-            yield return null;
+        if (gameObject.name == "Sky Background" && transform.position.y <= 0)
+        {
+            speed = 0;
         }
     }
+
+    private void MoveDown()
+    {
+        transform.Translate(Vector3.down * speed * Time.deltaTime);
+    }
+    
 }
