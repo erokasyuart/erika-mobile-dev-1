1df527b4c1fef2a26b0a9a6e86f29ab1d7e50d94
Erika Stuart <erikat.stuart@gmail.com>
2024-11-12 05:11:59 +1300

    Fixed boundary

---
+++ Assets/Scripts/PlayerBoundary.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBoundary : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> boundaryObjects = new List<GameObject>();
+    private Vector3 boundaryPosition;
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < boundaryObjects.Count; i++)
+        {
+            boundaryPosition = boundaryObjects[i].transform.position;
+            boundaryPosition.y = transform.position.y;
+            boundaryObjects[i].transform.position = boundaryPosition;
+            
+        }
+    }
+}
